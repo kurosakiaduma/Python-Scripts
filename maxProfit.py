186ae1a3ff40ffe9649c8c5bce518b6743ebec5b
Tevin Aduma <tevin74@live.com>
2022-12-12 10:25:30 +0300

    working on fixing return value from optimizer

--- maxProfit.py
+++ maxProfit.py
@@ -3,13 +3,13 @@ class Solution:
         day = prices.index(min(prices))
         alt_prices = prices[day:]
         prices = prices[:day]
-
         print(prices, alt_prices, day)
         
+        print("I am here now", prices, alt_prices, day)
         if len(alt_prices) < 1:
             print(f"I made it here! {alt_prices}")
             prices = alt_prices
-        elif prices and ((max(prices) - min(prices)) > (max(alt_prices) - min(alt_prices))) and (prices.index(max(prices)) !=0):
+        elif prices and ((max(prices) - min(prices)) > (max(alt_prices) - min(alt_prices))) and (prices.index(max(prices)) !=0 or (min(prices)!=prices[-1])):
             print("Need to re-shuffle")
             if len(prices) > 2 and (prices[0]!=min(prices) or prices[-1]!=max(prices)):
                 print("Optimizing")
@@ -20,6 +20,8 @@ class Solution:
                 return (max(prices) - min(prices))
         else:
             print("NO SHUFFLE")
-            prices = alt_prices
-            print(prices)
-            return (max(prices) - min(prices))
+            print(alt_prices)
+            return(max(alt_prices) - min(alt_prices))
+        
+        print("I am here now at the end",prices, alt_prices)
+        return (max(prices) - min(prices))
