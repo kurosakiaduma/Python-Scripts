901ca18560656fc60ca9aed30275bfccc3ce18cf
Tevin Aduma <tevin74@live.com>
2022-12-27 18:49:37 +0300

    add new program for adding a set of predefined numbers in the array

---
+++ highestSum.py
@@ -0,0 +1,22 @@
+'''
+Traversing through the current subarray sum
+'''
+
+def findcurrentSum(arr: list[int], k) -> int:
+    
+    currentSum = 0
+    
+    highestSum = 0
+    
+    i = 0
+    
+    while (i+(k-1)) < len(arr):
+        currentSum = sum(arr[i:i+k])
+        if currentSum != 0:
+            highestSum = max(currentSum, highestSum)
+        i+=1
+    
+    return highestSum
+
+if __name__ == "__main__":
+    findcurrentSum([1,3,4,5,8,5,2,9,5,7,0,8,6,9,4,3], 3)
